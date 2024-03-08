fabb9c2f97b02419f50f793f67b6fb37c06fd301
MIN <pristo24631@gmail.com>
2024-03-08 22:52:53 +0900

    섹션 5까지 완료

--- src/main/java/com/example/inflearnspring01/controller/MemberController.java
+++ src/main/java/com/example/inflearnspring01/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.inflearnspring01.controller;
 
+import com.example.inflearnspring01.domain.Member;
 import com.example.inflearnspring01.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +19,30 @@ public class MemberController {
     // (따로 new MemberService()를 하지 않아도 됨)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    // Get 방식으로 mapping할 때 사용됨(get은 주로 조회하는 기능)
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    // Post 방식으로 mapping할 때 사용됨(post는 주로 어떤 데이터를 등록하는 기능)
+    public String create(MemberForm form) {
+        // form.name에 해당 페이지에서 등록한 이름이 저장됨
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
